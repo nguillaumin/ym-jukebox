d9858574c560171ccef2e48063bb996b62e83110
Nicolas Guillaumin <nicolas+git@guillaumin.me>
2024-09-18 19:41:24 +0200

    Set base path to have relative URLs in the production build

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
